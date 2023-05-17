ee692f37c84291e1f48d4eb6a6246e0bd0af0dff
Gerardo Cianciulli <gerardo.cianciulli@gmail.com>
2023-05-17 19:18:32 -0400

    css transition

--- launch-date.js
+++ launch-date.js
@@ -7,20 +7,53 @@ $(document).ready(function(){
         launchSecond = "00"
         launchDate = new Date(launchMonth + ' ' + launchDate + "," + launchYear + " " + launchHour + ":" + launchMinute + ":" + launchSecond), // default time is 00:00:00 for midnight
         countdownDate = launchDate.getTime(),
+        timeLeft = undefined,
+        daysLeft = undefined,
+        hoursLeft = undefined,
+        minutesLeft = undefined,
+        secondsLeft = undefined,
         updateTimer = function(){
-          var now = new Date,
-            timeLeft = countdownDate - now.getTime(),
-            daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24)),
-            hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-            minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
-            secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+          var newTimeLeft = countdownDate - new Date,
+          newDaysLeft = Math.floor(newTimeLeft / (1000 * 60 * 60 * 24)),
+          newHoursLeft = Math.floor((newTimeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+          newMinutesLeft = Math.floor((newTimeLeft % (1000 * 60 * 60)) / (1000 * 60)),
+          newSecondsLeft = Math.floor((newTimeLeft % (1000 * 60)) / 1000);
       
-          $("#clock-days").find(".clock-digits").text(daysLeft);
-          $("#clock-hours").find(".clock-digits").text(hoursLeft);
-          $("#clock-minutes").find(".clock-digits").text(minutesLeft);
-          $("#clock-seconds").find(".clock-digits").text(secondsLeft);
+          if(daysLeft != newDaysLeft) {
+            $("#clock-days").find(".clock-digits").text(newDaysLeft);
+            daysLeft = newDaysLeft;
+            $("#clock-days").find(".clock-perspective-plane").addClass("back");
+          }
+
+          if(hoursLeft != newHoursLeft) {
+            $("#clock-hours").find(".clock-digits").text(newHoursLeft);
+            hoursLeft = newHoursLeft;
+            $("#clock-hours").find(".clock-perspective-plane").addClass("back");
+          }
+
+          if(minutesLeft != newMinutesLeft) {
+              $("#clock-minutes").find(".clock-digits").text(newMinutesLeft);
+              minutesLeft = newMinutesLeft
+            $("#clock-minutes").find(".clock-perspective-plane").addClass("back");
+          }
+
+          if(secondsLeft != newSecondsLeft) {
+            $("#clock-seconds").find(".clock-digits").text(newSecondsLeft);
+            // $($("#clock-seconds").find(".clock-perspective-plane").children()[0]).text(secondsLeft)
+            secondsLeft = newSecondsLeft;
+            $("#clock-seconds").find(".clock-perspective-plane").show();
+
+            $("#clock-seconds").find(".clock-perspective-plane").addClass("back");
+
+          }
+console.log("addClass")
+          setTimeout(function(){
+            $(".clock-perspective-plane").removeClass("back");
+            $(".clock-perspective-plane").hide();
+      },300);
         };
 
         updateTimer();
         setInterval(updateTimer, 1000);
 });
+
